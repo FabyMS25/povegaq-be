fb3945f66ecc5dc49ab0aede979b2fda2245ddd4
Edgar Mamani <edggmym@gmail.com>
2025-04-29 14:24:03 -0400

    lista de emision por tipo parametro

--- src/main/java/com/gamq/ambiente/dto/mapper/TipoParametroMapper.java
+++ src/main/java/com/gamq/ambiente/dto/mapper/TipoParametroMapper.java
@@ -3,6 +3,8 @@ package com.gamq.ambiente.dto.mapper;
 import com.gamq.ambiente.dto.TipoParametroDto;
 import com.gamq.ambiente.model.TipoParametro;
 
+import java.util.stream.Collectors;
+
 public class TipoParametroMapper {
     public static TipoParametroDto toTipoParametroDto(TipoParametro tipoParametro){
         return new TipoParametroDto()
@@ -11,7 +13,11 @@ public class TipoParametroMapper {
                 .setDescripcion(tipoParametro.getDescripcion())
                 .setUnidad(tipoParametro.getUnidad())
                 .setActivo(tipoParametro.isActivo())
-                .setEstado(tipoParametro.isEstado());
+                .setEstado(tipoParametro.isEstado())
+                .setLimiteEmisionDtoList(tipoParametro.getLimiteEmisionList().stream().map( limiteEmision -> {
+                    return LimiteEmisionMapper.toLimiteEmisionDto(limiteEmision);
+                }).collect(Collectors.toList()))
+                ;
     }
     public static TipoParametro toTipoParametro(TipoParametroDto tipoParametroDto){
         return new TipoParametro()
