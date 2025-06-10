be3ab5fbf3370a3a0034bc5ed25d665b4cd2757e
Edgar Mamani <edggmym@gmail.com>
2025-06-10 16:09:02 -0400

    procesar vencimientos

--- src/main/java/com/gamq/ambiente/controller/VencimientoController.java
+++ src/main/java/com/gamq/ambiente/controller/VencimientoController.java
@@ -2,6 +2,7 @@ package com.gamq.ambiente.controller;
 
 import com.gamq.ambiente.dto.response.Response;
 import com.gamq.ambiente.scheduler.NotificacionScheduledTask;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/vencimientos")
 public class VencimientoController {
+    @Autowired
     NotificacionScheduledTask notificacionScheduledTask;
 
     @PostMapping("/procesar-vencimiento")
